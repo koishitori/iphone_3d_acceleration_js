e2842e4b59c74c2435cb6bfcb4aed167545782aa
小石取り <b1006039@gmail.com>
2019-02-20 06:52:11 +0900

    fix and add index_null

--- iphone_3d_acceleration.js
+++ iphone_3d_acceleration.js
@@ -4,12 +4,12 @@ class iphone_3d_acceleration{
     if(ua.indexOf('iPhone') > -1 || ua.indexOf('iPad') > -1 || ua.indexOf('iPod')  > -1){
       window.addEventListener('devicemotion', function(event) {
       	var gv = event.accelerationIncludingGravity;
-      	x = gv.x;
-      	y = gv.y;
-      	z = gv.z;
-      	this.rotX=Math.atan(x/z)/Math.PI*180;
-      	this.rotY=Math.atan(y/z)/Math.PI*180;
-      	this.rotZ=Math.atan(x/y)/Math.PI*180;
+      	this.gx = gv.x;
+      	this.gy = gv.y;
+      	this.gz = gv.z;
+      	this.rotX=Math.atan(this.gx/this.gz)/Math.PI*180;
+      	this.rotY=Math.atan(this.gy/this.gz)/Math.PI*180;
+      	this.rotZ=Math.atan(this.gx/this.gy)/Math.PI*180;
         do_action();
       }.bind(this));
     }
