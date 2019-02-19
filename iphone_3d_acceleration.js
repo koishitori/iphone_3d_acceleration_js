4a2ceaeff99c4ba788e975095dfba2afcdac68d5
小石取り <b1006039@gmail.com>
2019-02-20 04:59:35 +0900

    Update iphone_3d_acceleration.js

---
+++ iphone_3d_acceleration.js
@@ -0,0 +1,21 @@
+class iphone_3d_acceleration{
+  constructor(){
+    var ua = navigator.userAgent;
+    if(ua.indexOf('iPhone') > -1 || ua.indexOf('iPad') > -1 || ua.indexOf('iPod')  > -1){
+      window.addEventListener('devicemotion', function(event) {
+      	var gv = event.accelerationIncludingGravity;
+      	x = gv.x;
+      	y = gv.y;
+      	z = gv.z;
+      	this.rotX=Math.atan(x/z)/Math.PI*180;
+      	this.rotY=Math.atan(y/z)/Math.PI*180;
+      	this.rotZ=Math.atan(x/y)/Math.PI*180;
+        do_action();
+      });
+    }
+  }
+
+  function do_action(){
+    return 0;
+  }
+}
